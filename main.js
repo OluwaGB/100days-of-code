5d98ebffa0edc9d9fa36fbe37eb1389b0e70a387
I T_code <oluwagbotemioyeboade@gmail.com>
2023-10-01 20:22:09 +0200

    Todo list.js
    JavaScript for todo list  
    Make use of array push method to add the new task to the screen ……..and array pop method to remove the last task from the screen

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log('Hello World!');
+todo = ["Design Service page ","Wash plate","testing"]
+newList = todo.pop();
+
+console.log(todo);
+
+btn = document.getElementById("btn");
+btn2 = document.getElementById("btn2");
+newTask = document.getElementById("list");
+
+display = document.getElementById("screen");
+ task = ["task1",];
+ display.value = task;
+ 
+ btn.onclick = function add(){ rew = newTask.value;
+ task.push(rew);
+ display.value = task;
+   
+ }
+ btn2.onlick = function subtract(){
+   brew = newTask.value;
+   task.pop(brew);
+   display.value = task;
+ }
+ 
+ b = 15e7;
+ c = 16e3;
+ 
+ 
+ a = document.write(14 + 104 );
+ console.log(b);
+ console.log(c);
